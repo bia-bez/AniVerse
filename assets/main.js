3adebbf68091c5e01db7255333f9116447a44f91
bianca-de-oliveira <131112317+bianca-de-oliveira@users.noreply.github.com>
2026-01-21 07:58:49 -0300

    feat: implement modal login functionality and styles

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,19 @@
 const buttonLogin = document.querySelector('#button-login');
-const mensagem = document.querySelector('#mensagem-login');
 
 buttonLogin.addEventListener('click', function () {
-  mensagem.textContent = 'Login em desenvolvimento';
-  mensagem.style.display = 'block';
+  const modalId = buttonLogin.getAttribute('data-modal');
+  const modal = document.getElementById(modalId);
+
+  modal.showModal();
+  
+});
+
+const closeButtons = document.querySelector('.close-modal');
+
+closeButtons.addEventListener('click', function () {
+  const modalId = buttonLogin.getAttribute('data-modal');
+  const modal = document.getElementById(modalId);
+
+  modal.close();
+  
 });
