3b3ebb71788a7e4094b1adab07b95db886d47f93
bianca-de-oliveira <131112317+bianca-de-oliveira@users.noreply.github.com>
2026-02-14 23:41:51 -0300

    feat: versão inicial em JavaScrip

--- assets/main.js
+++ assets/main.js
@@ -1,19 +1,17 @@
-const buttonLogin = document.querySelector('#button-login');
+const buttonLogin = document.querySelector("#button-login");
 
-buttonLogin.addEventListener('click', function () {
-  const modalId = buttonLogin.getAttribute('data-modal');
+buttonLogin.addEventListener("click", function () {
+  const modalId = buttonLogin.getAttribute("data-modal");
   const modal = document.getElementById(modalId);
 
   modal.showModal();
-  
 });
 
-const closeButtons = document.querySelector('.close-modal');
+const closeButtons = document.querySelector(".close-modal");
 
-closeButtons.addEventListener('click', function () {
-  const modalId = buttonLogin.getAttribute('data-modal');
+closeButtons.addEventListener("click", function () {
+  const modalId = buttonLogin.getAttribute("data-modal");
   const modal = document.getElementById(modalId);
 
   modal.close();
-  
 });
